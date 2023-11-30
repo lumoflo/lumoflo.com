964a80bc110965626ab01dd89b8fcf1386323a47
Prakhar Shukla <imprakharshukla@gmail.com>
2023-11-30 10:58:05 +0530

    add: more instagram graph api interactions

--- apps/admin/src/app/api/status/route.ts
+++ apps/admin/src/app/api/status/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
 
-import { getParentAndChildrenMediaFromInstagram } from "@gramflow/instagram";
-
 export const GET = async (req: Request) => {
   return NextResponse.json({
     message: "Hello from the API",
--- packages/instagram/instagram_graph_api_parser.ts
+++ packages/instagram/instagram_graph_api_parser.ts
@@ -1,3 +1,4 @@
+import { lutimes } from "fs";
 import { z } from "zod";
 
 enum MediaTypeEnum {
@@ -33,12 +34,10 @@ export const DaumSchema = z.object({
   permalink: z.string(),
 });
 
-export const PagingSchema = z
-  .object({
-    cursors: CursorsSchema,
-    next: z.string().optional(),
-  })
-  
+export const PagingSchema = z.object({
+  cursors: CursorsSchema,
+  next: z.string().optional(),
+});
 
 export const RootSchema = z.object({
   data: z.array(DaumSchema),
@@ -69,10 +68,14 @@ export async function getAllParentMediaFromInstagram({
     "timestamp",
   ],
   accessToken,
+  postPermalink,
+  limit = 100,
 }: {
   fields?: string[];
   accessToken: string;
   postNumber?: number;
+  postPermalink?: string;
+  limit?: number;
 }): Promise<MediaData[]> {
   const mediaData: MediaData[] = [];
   let nextPage:
@@ -80,16 +83,37 @@ export async function getAllParentMediaFromInstagram({
     | null = `https://graph.instagram.com/me/media?fields=${fields.join(
     ",",
   )}&access_token=${accessToken}`;
+
+  // If we have a postPermaLink, then we need to get the specific post instead of all posts
+
+  let limitNumber = 0;
   while (nextPage) {
     const response = await fetch(nextPage);
     const dataFromApi = await response.json();
     const initialData = RootSchema.parse(dataFromApi);
     const { data } = initialData;
     const extractedData = extractData(data); // Extract data from the initial response
-
     if (data) {
       if (extractedData && extractedData.length > 0) {
-        mediaData.push(...extractedData);
+        if (postPermalink && !limit) {
+          const foundPost = extractedData.find(
+            (item) => item.permalink === postPermalink,
+          );
+          if (foundPost) {
+            return [foundPost];
+          }
+        } else {
+          console.log({
+            limitNumber,
+            limit,
+          });
+          if (limitNumber <= limit) {
+            mediaData.push(...extractedData.slice(0, limit - limitNumber));
+            limitNumber += extractedData.length;
+          } else {
+            return mediaData;
+          }
+        }
       }
       if (initialData.paging && initialData.paging.next) {
         nextPage = initialData.paging.next;
@@ -139,6 +163,53 @@ export async function getChildrenMediaFromInstagram({
   }
   return mediaData;
 }
+
+export const getSpecificNumberOfPostsFromInstagram = async ({
+  accessToken,
+  postLimit = 100,
+}: {
+  accessToken: string;
+  postLimit?: number;
+}) => {
+  const parentMedia = await getAllParentMediaFromInstagram({
+    accessToken,
+    limit: postLimit,
+  });
+  return parentMedia;
+};
+
+export const searchPostsFromInstagram = async ({
+  accessToken,
+  postPermalink,
+  includeChildren = false,
+}: {
+  accessToken: string;
+  postPermalink: string;
+  includeChildren?: boolean;
+}) => {
+  const parentMedia = await getAllParentMediaFromInstagram({
+    accessToken,
+    postPermalink,
+  });
+  console.log("parentMedia:", parentMedia);
+  if (!includeChildren) {
+    return parentMedia;
+  }
+  const parentMediaWithChildren = await Promise.all(
+    parentMedia.map(async (item) => {
+      const childrenMedia = await getChildrenMediaFromInstagram({
+        accessToken,
+        parentPostId: item.id,
+        parentCaption: item.caption,
+      });
+      return {
+        ...item,
+        childrenMedia,
+      };
+    }),
+  );
+  return parentMediaWithChildren;
+};
 
 export const getParentAndChildrenMediaFromInstagram = async ({
   limit = 100,
