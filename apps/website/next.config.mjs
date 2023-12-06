ee7bc97acc90dc7d03f9086d28d6c554e35db8c1
areeburrub <areeburrub@gmail.com>
2023-12-06 15:31:18 +0530

    add: prisma workaround

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -4,7 +4,7 @@ import "./src/env.mjs";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+import PrismaPlugin from '@prisma/nextjs-monorepo-workaround-plugin'
 
 /** @type {import("next").NextConfig} */
 const config = {
