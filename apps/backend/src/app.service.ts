a5f2e2b03de8b3fcc1b4ca0883e520d69c3f6959
Prakhar <dev@prakharshukla.dev>
2024-01-01 22:36:47 +0530

    update:test

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Yay! I am alive and well! 🎉';
+    return 'Yay!! I am alive and well! 🎉';
   }
 }
